eb9f8f0cb9a530cb387f1e492007b62f57fee4fe
MAGS <120091142+Mercantec-MAGS@users.noreply.github.com>
2023-04-24 14:46:06 +0200

    Add project files.

---
+++ H3-Math/Models/Calculation.cs
@@ -0,0 +1,11 @@
+﻿namespace H3_Math.Models
+{
+    public class Calculation
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Result { get; set; }
+        public string Operation { get; set; }
+        public int UserNum { get; set; }
+    }
+}
